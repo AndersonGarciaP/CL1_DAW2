c8ea7649f7bbe34bb0444d24034a91db12878bb6
ANDERSON <i202020454@cibertec.edu.pe>
2023-05-06 21:50:14 -0500

    Initial commit

---
+++ src/test/java/com/example/cl1/daw2/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cl1.daw2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
